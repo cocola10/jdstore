e75ec805b222c8dc37a3ec00595cb63b01c614d8
haoran <haoran@haorandeMac-mini.local>
2017-05-25 14:50:51 +0800

    implement carts index function

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 u = User.new
 u.email = "admin@test.com"
 
@@ -14,3 +15,11 @@ u.password_confirmation = "111222"
 
 u.is_admin = true
 u.save
+
+Product.create!(
+title: "ipad",
+description: "ipad",
+price: 1999,
+quantity: 5,
+image: open("https://images-cn.ssl-images-amazon.com/images/I/61WqXnVh71L._SL1013_.jpg")
+)
